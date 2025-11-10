cda2b1762977df415b9dd75d5271a832e1cf61c4
Moh. Andrew Aprianto Putera <andrewaprianto@Mohs-MacBook-Pro.local>
2025-11-10 19:08:28 +0700

    Added Trailers Section and Small Fix Featured Section

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -21,7 +21,7 @@ const FeaturedSection = () => {
         </button>
       </div>
 
-      <div className="flex flex-wrap max-sm:justify-between gap-8">
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
         {dummyShowsData.slice(0, 4).map((show) => (
           <MovieCard key={show._id} movie={show} />
         ))}
