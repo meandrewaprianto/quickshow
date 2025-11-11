f7763a25cbdec6e199e8910b6a9f31a248a9c7a0
Moh. Andrew Aprianto Putera <andrewaprianto@Mohs-MacBook-Pro.local>
2025-11-11 07:59:22 +0700

    Finish Footer Section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,59 @@
+import { assets } from "../assets/assets";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="px-6 pt-8 md:px-16 lg:px-36 mt-40 w-full text-gray-300">
+      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
+        <div className="md:max-w-96">
+          <img src={assets.logo} alt="" class="w-36 h-auto" />
+          <p className="mt-6 text-sm">
+            Lorem ipsum has been the industry's standard dummy text ever since
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book.
+          </p>
+          <div className="flex items-center gap-2 mt-4">
+            <img
+              src={assets.googlePlay}
+              alt="google play"
+              className="h-9 w-auto"
+            />
+            <img src={assets.appStore} alt="app store" className="h-9 w-auto" />
+          </div>
+        </div>
+
+        <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
+          <div>
+            <h2 className="font-semibold mb-5">Company</h2>
+            <ul className="text-sm space-y-2">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-5">Get in touch</h2>
+            <div className="text-sm space-y-2">
+              <p>+1-234-567-980</p>
+              <p>contact@example.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="pt-4 text-center text-sm pb-5">
+        Copyright {new Date().getFullYear()} &copy; All Right Reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
